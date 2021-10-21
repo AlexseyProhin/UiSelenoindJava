d993905014602442ca3d84e10cc49fadd95aa2ca
AlexseyProhin <alexsey.prohin@yandex.ru>
2021-10-21 15:44:49 +0300

    Тест написан, но есть ошибка запуска

---
+++ src/test/java/com/github/qa/unittests/githubTest.java
@@ -0,0 +1,45 @@
+package com.github.qa.unittests;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class githubTest {
+    private WebDriver driver;
+
+    @BeforeAll
+    public static void chekbrousershin() {
+        WebDriverManager.chromedriver().setup();
+    }
+
+    @BeforeEach
+    public void setUp() {
+    driver = new ChromeDriver();
+    driver.get("https://github.com/AlexseyProhin");
+
+    }
+
+    @AfterEach
+    public void tearDown() {
+        if (driver != null) {
+            driver.quit();
+        }
+    }
+
+
+    @Test
+    public void simpleTest() {
+        String xpath = "//input[@class='form-control input-sm header-search-input jump-to-field js-jump-to-field js-site-search-focus']";
+        WebElement inputElement = driver.findElement(By.xpath(xpath));
+        inputElement.sendKeys("Hello World");
+        inputElement.submit();
+
+
+    }
+}
